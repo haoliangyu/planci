9a64e58817ef2d009299451cbe26a3f484cadb94
Haoliang Yu <hyu@esri.com>
2025-03-02 23:24:57 -0500

    add beta footer

--- packages/calculator-web/src/App.js
+++ packages/calculator-web/src/App.js
@@ -78,7 +78,7 @@ function App() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r overflow-y-auto max-w-3xl mx-auto">
       <header className="w-full bg-white fixed top-0 shadow-xl">
         <div className="container mx-auto px-4 py-4 flex items-center">
-          <h1 className="text-xl font-bold">Planci</h1>
+          <h1 className="text-xl font-bold">Planci <span className="text-sm font-medium text-gray-500">Beta</span></h1>
         </div>
       </header>
       <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row justify-center space-y-8 md:space-y-0 md:space-x-8 mt-16">
