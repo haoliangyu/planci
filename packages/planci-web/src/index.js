55802621953caef34332d4700753b3de60bc8d16
Haoliang Yu <hyu@esri.com>
2025-03-09 01:15:08 -0500

    add speedinsight module

--- packages/planci-web/src/index.js
+++ packages/planci-web/src/index.js
@@ -4,12 +4,14 @@ import './index.css'; // Ensure this import is present
 import App from './App';
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const container = document.getElementById('root');
 const root = createRoot(container); // Create a root
 root.render(
   <React.StrictMode>
     <Analytics/>
+    <SpeedInsights/>
     <App />
   </React.StrictMode>
 );
